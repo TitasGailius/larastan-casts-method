2d7b1be16d612ea44ba3e0af59c06a8c57abfe20
Titas Gailius <titas.gailius@gmail.com>
2025-06-05 16:22:16 +0200

    Init

---
+++ src/ModelCastsExtension.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace TitasGailius\LarastanCastsMethod;
+
+use Illuminate\Database\Eloquent\Model;
+use Larastan\Larastan\Properties\ModelCastHelper as LarastanModelCastHelper;
+use Larastan\Larastan\Properties\ModelProperty;
+use Larastan\Larastan\Properties\ModelPropertyHelper;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\PropertiesClassReflectionExtension;
+use PHPStan\Reflection\PropertyReflection;
+use PHPStan\ShouldNotHappenException;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\UnionType;
+use ReflectionException;
+use ReflectionMethod;
+
+/**
+ * Custom extension that extends Larastan's ModelPropertyExtension.
+ *
+ * This extension runs the `casts` method on the Model class when inspecting model properties,
+ * which eliminates the need to explicitly define return array shapes for the `casts` method.
+ * By extending Larastan's ModelPropertyExtension, we can automatically infer property types
+ * from the model's cast definitions at runtime.
+ */
+class ModelCastsExtension implements PropertiesClassReflectionExtension
+{
+    /**
+     * Cached model casts.
+     *
+     * @var array<class-string, array<string, string>>
+     */
+    protected array $casts = [];
+
+    /**
+     * Instantiate a new helper instance.
+     */
+    public function __construct(
+        protected ModelPropertyHelper $modelPropertyHelper,
+        protected LarastanModelCastHelper $modelCastHelper,
+    ) {}
+
+    /**
+     * Determine if the property exists.
+     */
+    public function hasProperty(ClassReflection $classReflection, string $propertyName): bool
+    {
+        return $this->modelPropertyHelper->hasDatabaseProperty($classReflection, $propertyName)
+            && isset($this->getModelCasts($classReflection)[$propertyName]);
+    }
+
+    /**
+     * Get casted property.
+     */
+    public function getProperty(ClassReflection $classReflection, string $propertyName): PropertyReflection
+    {
+        $cast = $this->getModelCasts($classReflection)[$propertyName];
+
+        $original = $this->modelPropertyHelper->getDatabaseProperty($classReflection, $propertyName);
+
+        $readableType = $this->modelCastHelper->getReadableType($cast, $original->getReadableType());
+        $writableType = $this->modelCastHelper->getWriteableType($cast, $original->getWritableType());
+
+        if ($this->isNullable($original->getWritableType())) {
+            $readableType = TypeCombinator::addNull($readableType);
+            $writableType = TypeCombinator::addNull($writableType);
+        }
+
+        return new ModelProperty($classReflection, $readableType, $writableType);
+    }
+
+    /**
+     * Get model casts.
+     *
+     * @return array<string, string>
+     */
+    protected function getModelCasts(ClassReflection $reflection): array
+    {
+        if (! $reflection->is(Model::class)) {
+            return [];
+        }
+
+        if ($reflection->isAbstract()) {
+            return [];
+        }
+
+        if (isset($this->casts[$reflection->getName()])) {
+            return $this->casts[$reflection->getName()];
+        }
+
+        try {
+            /** @var Model $modelInstance */
+            $modelInstance = $reflection->getNativeReflection()->newInstanceWithoutConstructor();
+        } catch (ReflectionException) {
+            throw new ShouldNotHappenException;
+        }
+
+        // @phpstan-ignore-next-line
+        (new ReflectionMethod($modelInstance, 'initializeHasAttributes'))->invoke($modelInstance);
+
+        return $this->casts[$reflection->getName()] = $modelInstance->getCasts();
+    }
+
+    /**
+     * Determine if the given type is nullable.
+     */
+    protected function isNullable(Type $type): bool
+    {
+        $types = $type instanceof UnionType
+            ? $type->getTypes()
+            : [$type];
+
+        foreach ($types as $type) {
+            if ($type->isNull()->yes()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
